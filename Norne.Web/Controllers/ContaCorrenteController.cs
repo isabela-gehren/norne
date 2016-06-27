42e6dd9672246502ec5144a4cb6e04663435c4dc
Diogo Paraíso <foxfrito@gmail.com>
2016-06-27 17:23:40 -0300

    Criação da tela de abertura de conta.

---
+++ Norne.Web/Controllers/ContaCorrenteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Norne.Web.Controllers
+{
+    public class ContaCorrenteController : Controller
+    {
+        // GET: ContaCorrente
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
