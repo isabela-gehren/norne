f8d2a1d4502c9cd2ef49667f310939028e1a6cb3
Isabela Gehren <Isabela Gehren>
2016-06-27 22:49:56 -0300

    .

--- Norne.Web/Controllers/ContaCorrenteController.cs
+++ Norne.Web/Controllers/ContaCorrenteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Norne.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,30 @@ namespace Norne.Web.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public void Create(ContaCorrente conta)
+        {
+            //ContaCorrenteBusiness cc = new ContaCorrenteBusiness();
+            //var conta = new ContaCorrente();
+            //conta.Agencia = new Agencia() { Codigo = 9 };
+            //var statatusbusiness = new StatusContaBusiness();
+            //conta.StatusConta = statatusbusiness.Listar().FirstOrDefault();
+            //conta.CorrentistaTitular = business.Listar().FirstOrDefault();
+            //cc.Incluir(conta);
+
+            //ContaPoupancaBusiness cp = new ContaPoupancaBusiness();
+            //var conta2 = new ContaPoupanca();
+            //conta2.Agencia = new Agencia() { Codigo = 9 };
+            //conta2.Codigo = 12;
+            //conta2.Status = statatusbusiness.Listar().FirstOrDefault();
+            //conta2.CorrentistaTitular = business.Listar().FirstOrDefault();
+            //cp.Incluir(conta2);
+
+            //ContaCorrenteBusiness cc = new ContaCorrenteBusiness();
+            //var contascorrentes = cc.Listar();
+
         }
     }
 }
--- Norne.Web/Controllers/CorrentistasController.cs
+++ Norne.Web/Controllers/CorrentistasController.cs
@@ -19,25 +19,6 @@ namespace Norne.Web.Controllers
         // GET: Correntistas
         public ActionResult Index()
         {
-            //ContaCorrenteBusiness cc = new ContaCorrenteBusiness();
-            //var conta = new ContaCorrente();
-            //conta.Agencia = new Agencia() { Codigo = 9 };
-            //var statatusbusiness = new StatusContaBusiness();
-            //conta.StatusConta = statatusbusiness.Listar().FirstOrDefault();
-            //conta.CorrentistaTitular = business.Listar().FirstOrDefault();
-            //cc.Incluir(conta);
-
-            //ContaPoupancaBusiness cp = new ContaPoupancaBusiness();
-            //var conta2 = new ContaPoupanca();
-            //conta2.Agencia = new Agencia() { Codigo = 9 };
-            //conta2.Codigo = 12;
-            //conta2.Status = statatusbusiness.Listar().FirstOrDefault();
-            //conta2.CorrentistaTitular = business.Listar().FirstOrDefault();
-            //cp.Incluir(conta2);
-
-            //ContaCorrenteBusiness cc = new ContaCorrenteBusiness();
-            //var contascorrentes = cc.Listar();
-
             return View(business.Listar());
         }
 
